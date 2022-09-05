dd4b6614f01fdc7eb46c008b6080abb54b4934a7
USER <USER@WIN-QGII8ID360C>
2022-09-05 14:19:11 +0500

    Add project files.

---
+++ Kitap_beti/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Kitap_beti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bir = new System.Windows.Forms.RichTextBox();
+            this.eki = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Sani = new System.Windows.Forms.RichTextBox();
+            this.esaplaw = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ush = new System.Windows.Forms.RichTextBox();
+            this.tort = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Baslangish = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Baslangish)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bir
+            // 
+            this.bir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bir.Location = new System.Drawing.Point(157, 118);
+            this.bir.Name = "bir";
+            this.bir.Size = new System.Drawing.Size(631, 32);
+            this.bir.TabIndex = 14;
+            this.bir.Text = "";
+            // 
+            // eki
+            // 
+            this.eki.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eki.Location = new System.Drawing.Point(157, 168);
+            this.eki.Name = "eki";
+            this.eki.Size = new System.Drawing.Size(631, 32);
+            this.eki.TabIndex = 20;
+            this.eki.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(9, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 32);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ekewlik aldi";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label2.Location = new System.Drawing.Point(9, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 32);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ekewlik arti";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label3.Location = new System.Drawing.Point(9, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 32);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "To\'rtewlik aldi";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label4.Location = new System.Drawing.Point(9, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 32);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "To\'rtewlik arti";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Sani
+            // 
+            this.Sani.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sani.Location = new System.Drawing.Point(176, 17);
+            this.Sani.Name = "Sani";
+            this.Sani.Size = new System.Drawing.Size(57, 32);
+            this.Sani.TabIndex = 8;
+            this.Sani.Text = "";
+            // 
+            // esaplaw
+            // 
+            this.esaplaw.BackColor = System.Drawing.Color.Lime;
+            this.esaplaw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.esaplaw.Location = new System.Drawing.Point(262, 17);
+            this.esaplaw.Name = "esaplaw";
+            this.esaplaw.Size = new System.Drawing.Size(94, 32);
+            this.esaplaw.TabIndex = 10;
+            this.esaplaw.Text = "Esaplaw";
+            this.esaplaw.UseVisualStyleBackColor = false;
+            this.esaplaw.Click += new System.EventHandler(this.esaplaw_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label5.Location = new System.Drawing.Point(51, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 23);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Betler sani";
+            // 
+            // ush
+            // 
+            this.ush.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ush.Location = new System.Drawing.Point(157, 300);
+            this.ush.Name = "ush";
+            this.ush.Size = new System.Drawing.Size(631, 32);
+            this.ush.TabIndex = 17;
+            this.ush.Text = "";
+            // 
+            // tort
+            // 
+            this.tort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tort.Location = new System.Drawing.Point(157, 350);
+            this.tort.Name = "tort";
+            this.tort.Size = new System.Drawing.Size(631, 32);
+            this.tort.TabIndex = 18;
+            this.tort.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label6.Location = new System.Drawing.Point(373, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(266, 32);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Nomeraciya baslang\'an bet\r\n";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Baslangish
+            // 
+            this.Baslangish.Location = new System.Drawing.Point(629, 24);
+            this.Baslangish.Name = "Baslangish";
+            this.Baslangish.Size = new System.Drawing.Size(75, 22);
+            this.Baslangish.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(840, 437);
+            this.panel1.TabIndex = 21;
+            this.panel1.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Yellow;
+            this.label7.Location = new System.Drawing.Point(177, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(462, 116);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Betler sani 4 ke eseli boliw kerek    (misali 4,8,16...)";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(308, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 46);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(836, 434);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Baslangish);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tort);
+            this.Controls.Add(this.ush);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.esaplaw);
+            this.Controls.Add(this.Sani);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.eki);
+            this.Controls.Add(this.bir);
+            this.MaximumSize = new System.Drawing.Size(854, 481);
+            this.Name = "Form1";
+            this.Text = "Kitap_Beti";
+            ((System.ComponentModel.ISupportInitialize)(this.Baslangish)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox bir;
+        private System.Windows.Forms.RichTextBox eki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox Sani;
+        private System.Windows.Forms.Button esaplaw;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox ush;
+        private System.Windows.Forms.RichTextBox tort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown Baslangish;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Kitap_beti/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitap_beti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n, m;
+        string a = "";
+        string b = "";
+        string c = "";
+        string d = "";
+
+        private void esaplaw_Click(object sender, EventArgs e)
+        {
+                a = "";
+                b = "";
+                c = "";
+                d = "";
+                Manis_beriw();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible=false;
+            
+        }
+
+        private void Manis_beriw()
+        {
+            try
+            {
+                n = int.Parse(Sani.Text);
+                m = int.Parse(Baslangish.Text);
+
+                if (n % 4 == 0)
+                {
+                    for (int i = n - 1 + m, j = m; (i > m - 1 + n / 2) && (j < m - 1 + n / 2); i = i - 2, j = j + 2)
+                    {
+                        a += i + ",";
+                        if (j == (m + n / 2 - 2))
+                            a += j.ToString();
+                        else
+                            a += j + ",";
+
+                    }
+
+                    for (int i = m + n / 2 - 1, j = m + n / 2; i > m - 1 && j < n + m; i = i - 2, j = j + 2)
+                    {
+                        b += i + ",";
+                        if (j == (m + n - 2))
+                            b += j.ToString();
+                        else
+                            b += j + ",";
+
+                    }
+
+                    if (n % 8 == 0)
+                    {
+                        for (int i = n - 1 + m, j = m; (i > m - 1 + n / 2) && (j < m - 1 + n / 2); i = i - 2, j = j + 2)
+                        {
+                            c += i + ",";
+                            if (j == (m + n / 2 - 2))
+                                c += j.ToString();
+                            else
+                                c += j + ",";
+
+                        }
+
+                        for (int i = (m + n / 2 - 3), j = m + n / 2 + 2, k = m + n / 2 - 1, l = m + n / 2; (i >= m - 1) && (j <= m + n - 2) && (k >= m + 3) && (l <= m + n - 4); i = i - 4, j = j + 4, k = k - 4, l = l + 4)
+                        {
+                            d += i + ",";
+                            d += j + ",";
+                            d += k + ",";
+                            if (l == (m + n - 4))
+                                d += l.ToString();
+                            else
+                                d += l + ",";
+
+                        }
+                    }
+                    else
+                    {
+                        c += "Betler sani 8 ge eseli boliwi kerek(8,16,24...)";
+                        d += "***********************************************";
+                    }
+                    bir.Text = a;
+                    eki.Text = b;
+                    ush.Text = c;
+                    tort.Text = d;
+                }
+                else
+                {
+                    panel1.Visible = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Siz pu'tin san kiritpedin'iz' ");
+            }
+        }
+        
+    }
+}
